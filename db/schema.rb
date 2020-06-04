a6bf1bf21d95cf9337d63118a6ec8b3b34da195d
sdcro <sdcrouse91@gmail.com>
2020-06-03 17:34:34 -0700

    Add category attribute to Ingredient model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_053204) do
+ActiveRecord::Schema.define(version: 2020_06_04_003323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_053204) do
     t.bigint "concoction_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
     t.index ["concoction_id"], name: "index_ingredients_on_concoction_id"
   end
 
