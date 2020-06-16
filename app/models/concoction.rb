6d74bc74706db6c321e6cb024f69b636650d4dc4
sdcro <sdcrouse91@gmail.com>
2020-06-15 23:21:11 -0700

    Have the Concoction model accept nested attributes for coffees, with edge case
    The edge case is when the user inputs only one coffee, and not 2, 3, etc.
    The blank coffee hash(es) will be silently rejected without an error.

--- app/models/concoction.rb
+++ app/models/concoction.rb
@@ -1,4 +1,7 @@
 class Concoction < ApplicationRecord
   has_many :coffees
   has_many :ingredients
+
+  accepts_nested_attributes_for :coffees, 
+    reject_if: proc{ |attrs| attrs[:amount].blank? && attrs[:brand].blank? && attrs[:variety].blank? }
 end
