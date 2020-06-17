77e6f016dbbfb678d648885fc7c4295ee2d0882e
sdcro <sdcrouse91@gmail.com>
2020-06-16 21:19:45 -0700

    Remove old code and comments from Concoctions#create action

--- app/controllers/api/v1/concoctions_controller.rb
+++ app/controllers/api/v1/concoctions_controller.rb
@@ -15,33 +15,7 @@ class Api::V1::ConcoctionsController < ApplicationController
   end
 
   def create
-    # params1 = {
-    #   concoction: {
-    #     name: "Random Concoction",
-    #     instructions: "Mix all coffees and ingredients. Drizzle with caramel. Serve hot.",
-    #     notes: "This is for coding purposes only",
-    #     coffees: [
-    #       {amount: "1 tsp", brand: "Folger's", variety: "Instant Classic Roast"},
-    #       {amount: "1 tsp", brand: "Yuban", variety: "Instant Coffee"}
-    #     ],
-    #     ingredients: [
-    #       {category: "liquid", amount: "1 cup", name: "hot water"},
-    #       {category: "sweetener", amount: "1 packet", name: "Sweet and Low"},
-    #       {category: "creamer", amount: "1-2 tsp", name: "Irish creamer"},
-    #       {category: "other", amount: "1/4 tsp", name: "Cinnamon"},
-    #       {category: "other", amount: "1/2 tsp", name: "Caramel"}
-    #     ]
-    #   }
-    # }
-
     concoction = Concoction.new(concoction_params)
-    puts concoction, concoction.name, concoction.instructions, concoction.notes
-
-    coffee = concoction.coffees[0]
-    puts coffee.amount, coffee.brand, coffee.variety
-
-    ingredient = concoction.ingredients[0]
-    puts ingredient.category, ingredient.amount, ingredient.name
 
     render json: concoction
   end
